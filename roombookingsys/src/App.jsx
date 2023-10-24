fa214f3cadfcd87938fbad452df0081d060ceae6
Brian Hugo <brian913555@gmail.com>
2023-10-24 18:21:50 +0800

    fixed vite issue and installed material ui into project

--- roombookingsys/src/App.jsx
+++ roombookingsys/src/App.jsx
@@ -1,10 +1,12 @@
 import "./styles/App.css";
 import { Button } from "@/components/button";
+import BoxComponent from "./components/box";
 
 function App() {
   return (
     <div>
       <Button>Click me</Button>
+      <BoxComponent></BoxComponent>
     </div>
   );
 }
