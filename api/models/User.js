58e1bd1e82d7c068e30ae32af67110c741ac6c4f
Brian Tan <brian913@live.com>
2023-11-05 15:55:01 +0800

    migrated from mongoDB to mysql

--- api/models/User.js
+++ api/models/User.js
@@ -1,13 +1,47 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+// const { Sequelize, DataTypes } = require("sequelize");
 
-const UserSchema = new Schema({
-  //creates a new schema for the user document
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
+// const sequelize = new Sequelize("database", "username", "password", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
+// // creates a new table for the user
+// const User = sequelize.define("User", {
+//   name: {
+//     type: DataTypes.STRING,
+//   },
+//   email: {
+//     type: DataTypes.STRING,
+//     unique: true,
+//   },
+//   password: {
+//     type: DataTypes.STRING,
+//   },
+// });
+
+// module.exports = User;
+
+const mysql = require("mysql2");
+
+// creates a connection to the database
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "screwthisshitS9922756G@",
+  database: "roombookingsys",
 });
 
-const UserModel = mongoose.model("User", UserSchema); //creates a new model for the user document
-
-module.exports = UserModel;
+const User = pool.query(
+  `
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
+  password VARCHAR(255)
+)
+`,
+  (err, results) => {
+    if (err) throw err;
+    console.log("Table created successfully");
+  }
+);
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,46 +1,71 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 import { UserContext } from "../UserContext";
+import { format } from "date-fns";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import ResourceCalendar from "./ResourceCalendar";
 
 export default function IndexPage() {
   const { user } = useContext(UserContext);
+
+  const [selected, setSelected] = useState(null);
+  const defaultMonth = new Date(2023, 10);
+
+  let footer = <p>Please pick a day.</p>;
+  if (selected) {
+    footer = <p>{format(selected, "PP")}</p>;
+  }
+
   return (
-    <div>
+    <div className="flex flex-col">
       <header className="flex justify-between">
         <a href="" className="flex items-center gap-3">
           <img
-            className="object-scale-down h-15 w-40"
+            className="object-scale-down h-36 w-36"
             src="https://uowplaybook.s3-ap-southeast-2.amazonaws.com/logo/logo-secondary.png"
           />
-          <button className="flex items-center font-semibold text-xl">
+          <button className="flex items-center font-medium text-xl">
             View Dashboard
           </button>
-          <button className="flex items-center font-semibold text-xl">
+          <button className="flex items-center font-medium text-xl">
             My Bookings
           </button>
         </a>
         <Link
           to={"/Login"}
-          className="font-bold border border-gray-200 rounded-full py-1.5 px-3 shadow-md shadow-gray-100 m-9 "
+          className="font-bold border border-gray-200 rounded-full py-1.5 px-3 shadow-md shadow-gray-100 m-14 "
         >
           Test loginpage
         </Link>
         {!!user && <div>{user.name}</div>}
-        <button className="font-bold border border-gray-200 rounded-full py-1.5 px-3 shadow-md shadow-gray-100 m-9 ">
+        <button className="font-bold border border-gray-200 rounded-full px-3 shadow-md shadow-gray-100 m-12 ">
           Logout
         </button>
       </header>
-      <div className="flex border border-gray-300 rounded-full p-2 gap-3 shadow-md shadow-gray-300">
-        <div className="bg-primary font-medium text-white p1 rounded-full w-10 h-7">
+      <div className="flex justify-center self-center border border-gray-300 rounded-full p-1 gap-3 shadow-md shadow-gray-300 mb-10 w-1/5">
+        <div className="font-medium bg-lime-700 text-white rounded-full w-10 h-7">
           Day
         </div>
-        <div className="border-l border-gray-300"></div>
-        <div>Week</div>
-        <div className="border-l border-gray-300"></div>
-        <div>Month</div>
-        <div className="border-l border-gray-300"></div>
-        <div>Year</div>
+        <div className="border-2 border-gray-200"></div>
+        <div className="font-medium">Week</div>
+        <div className="border-2 border-gray-200"></div>
+        <div className="font-medium">Month</div>
+        <div className="border-2 border-gray-200"></div>
+        <div className="font-medium">Year</div>
+      </div>
+      <div className="flex flex-wrap justify-between mx-5">
+        <DayPicker
+          mode="single"
+          selected={selected}
+          onSelect={setSelected}
+          footer={footer}
+          defaultMonth={defaultMonth}
+          fromMonth={defaultMonth}
+          toDate={new Date(2023, 12)}
+        />
+        <ResourceCalendar />
       </div>
     </div>
   );
